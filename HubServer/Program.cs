051c275a072c812956d023c1a71947afcd73322c
Marcin <rhoninn11@gmail.com>
2019-04-26 15:18:58 +0200

    initial commit

---
+++ HubServer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace HubServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPHostEntry entryPoint = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = entryPoint.AddressList[0];
+            IPEndPoint serverEndPoint = new IPEndPoint(ipAddress, 5454);
+
+
+            Socket serverSocket = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            serverSocket.Bind(serverEndPoint);
+            System.Console.WriteLine($"Sever is listning on: {serverEndPoint.Address}:{serverEndPoint.Port}");
+            serverSocket.Listen(3);
+
+            Socket clientSocket = serverSocket.Accept();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Projekt na SCADĘ
+
+Akwizycja danych pobierana od trzech różnych klientów. Następnie dane zosaję przetworzone i w przetworzonej formie przesłane dalej
+
+![alt text](extras/infopic.jpg)
