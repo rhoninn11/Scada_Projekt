c976dd5891879541d08255c60aa4d64a6b1469f8
Marcin <rhoninn11@gmail.com>
2019-06-01 14:48:12 +0200

    repo cleaning and basic client server connection

---
+++ ClientlikeSensor/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ClientlikeSensor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress address = null;
+            int portNUmber = 0;
+            int sensorId = 0;
+
+            if (args.Length < 3)
+            {
+                System.Console.WriteLine("Not enougth arguments:\n\r1: Ip addres\n\r2: Port number\n\r3: sensor id");
+                return;
+            }
+
+
+            try
+            {
+                address = IPAddress.Parse(args[0]);
+                portNUmber = Convert.ToInt32(args[1]);
+                sensorId = Convert.ToInt32(args[2]);
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine(e.Message);
+            }
+
+            IPHostEntry entryPoint = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = entryPoint.AddressList[0];
+            IPEndPoint serverEndPoint = new IPEndPoint(address, portNUmber);
+
+
+            Socket sensor = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                sensor.Connect(serverEndPoint);
+                string message = $"Hello form sensor {sensorId}\r";
+                byte[] buffer = Encoding.UTF8.GetBytes(message);
+                int bytesSend = sensor.Send(buffer);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
--- HubServer/Program.cs
+++ HubServer/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 namespace HubServer
 {
@@ -13,13 +14,48 @@ namespace HubServer
             IPEndPoint serverEndPoint = new IPEndPoint(ipAddress, 5454);
 
 
-            Socket serverSocket = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            Socket listner = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 
-            serverSocket.Bind(serverEndPoint);
-            System.Console.WriteLine($"Sever is listning on: {serverEndPoint.Address}:{serverEndPoint.Port}");
-            serverSocket.Listen(3);
+            try
+            {
+                listner.Bind(serverEndPoint);
+                listner.Listen(3);
 
-            Socket clientSocket = serverSocket.Accept();
+                Console.WriteLine($"Sever is listning on: {serverEndPoint.Address}:{serverEndPoint.Port}");
+
+                while (true)
+                {
+                    Console.WriteLine("Weatiting Connection");
+                    Socket clientlikeSensor = listner.Accept();
+
+                    byte[] buffer = new Byte[1024];
+                    String sBuffer = String.Empty;
+
+                    while (true)
+                    {
+                        int bytesRecived = clientlikeSensor.Receive(buffer);
+                        sBuffer += Encoding.UTF8.GetString(buffer, 0, bytesRecived);
+                        if(sBuffer.Contains('\r'))
+                            break;
+                    }
+
+                    Console.WriteLine($"Recived data: {sBuffer}");
+
+                    byte[] messageToSensor = Encoding.UTF8.GetBytes("Jest komunikcaja");
+
+                    clientlikeSensor.Send(messageToSensor);
+
+                    clientlikeSensor.Shutdown(SocketShutdown.Both);
+                    clientlikeSensor.Close();
+                }
+
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine(e.Message);
+            }
+
+            listner.Close();
         }
     }
 }
