a61ef88bdf76fcfcb879c4591a067a70f691724f
Marcin <rhoninn11@gmail.com>
2019-06-05 09:19:14 +0200

    prawiedziałająca wersja

--- HubServer/Program.cs
+++ HubServer/Program.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Linq;
+using Newtonsoft.Json;
 
 using ClientlikeSensor;
 
@@ -15,6 +16,11 @@ namespace HubServer
     {
         static void Main(string[] args)
         {
+            IPAddress address;
+            int portNumber;
+            if (ChcekArgs(args, out address, out portNumber) < 0)
+                return;
+
             IPHostEntry entryPoint = Dns.GetHostEntry(Dns.GetHostName());
             IPAddress ipAddress = entryPoint.AddressList[0];
             IPEndPoint serverEndPoint = new IPEndPoint(ipAddress, 5454);
@@ -34,11 +40,36 @@ namespace HubServer
                 return;
             }
 
-            ManagingService mg = new ManagingService(listner);
+            ManagingService mg = new ManagingService(listner, address, portNumber);
             mg.WaitFornewSensors();
 
 
 
+        }
+
+        public static int ChcekArgs(string[] args, out IPAddress address, out int portNUmber)
+        {
+            address = null;
+            portNUmber = 0;
+
+
+            if (args.Length < 2)
+            {
+                System.Console.WriteLine("Not enougth arguments:\n\r1: rasp ip\n\r2: Port number\n\r");
+                return -1;
+            }
+
+            try
+            {
+                address = IPAddress.Parse(args[0]);
+                portNUmber = Convert.ToInt32(args[1]);
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine(e.Message);
+                return -1;
+            }
+            return 1;
         }
     }
 
@@ -47,17 +78,28 @@ namespace HubServer
         Socket listner;
         List<SensorHandler> sensors = new List<SensorHandler>();
         Task readingTask;
+        Task dataSendingTask;
+        bool isFramReady = false;
+        JsonData SensorDataBuffer;
 
-        public ManagingService(Socket listner)
+        Socket rasp;
+
+        public ManagingService(Socket listner, IPAddress raspIp, int port)
         {
+            this.SensorDataBuffer = new JsonData();
             this.listner = listner;
+            this.ConnectToRasp(raspIp, port);
             this.readingTask = new Task(this.ReadAllSensorData);
             this.readingTask.Start();
+            this.dataSendingTask = new Task(this.sendingData);
+            this.dataSendingTask.Start();
 
         }
 
         public void WaitFornewSensors()
         {
+            if (this.rasp == null)
+                return;
             try
             {
                 Console.WriteLine("Weatiting Connection");
@@ -79,6 +121,9 @@ namespace HubServer
 
         public void ReadAllSensorData()
         {
+            if (this.rasp == null)
+                return;
+
             while (true)
             {
                 if (sensors.Count == 0)
@@ -89,21 +134,82 @@ namespace HubServer
                 {
                     StringBuilder info = new StringBuilder();
                     int i = 1;
-                    
+                    List<double> sensorValues = new List<double>();
                     sensors.ForEach(s =>
-                    {   
-                        while(s.signalQueue.Count > 1)
+                    {
+                        while (s.signalQueue.Count > 1)
                             s.signalQueue.Dequeue();
 
                         int count = s.signalQueue.Count;
                         SignalData data = s.signalQueue.Dequeue();
+                        sensorValues.Add(data.value);
                         info.Append($"sensor:{i++} wartość:{data.value}, timestamp:{data.timeSpan}, {count}\n");
                     });
+
+                    this.SensorDataBuffer.SensorData = sensorValues;
+                    double sum = 0;
+                    int c = sensorValues.Count;
+                    sensorValues.ForEach(v => sum += v);
+                    this.SensorDataBuffer.ProcessedData = sum/c;
+                    this.isFramReady = true;
+
                     System.Console.Clear();
                     System.Console.WriteLine(info.ToString());
                 }
-                
+
             }
+        }
+
+        public void sendingData()
+        {
+            if (this.rasp == null)
+                return;
+
+            while (true)
+            {
+
+                if(!this.isFramReady)
+                    continue;
+
+                byte[] message = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(this.SensorDataBuffer));
+                this.isFramReady = false;
+                this.rasp.Send(message);
+            }
+
+        }
+
+        public void ConnectToRasp(IPAddress address, int portNumber)
+        {
+            IPHostEntry entryPoint = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = entryPoint.AddressList[0];
+            IPEndPoint serverEndPoint = new IPEndPoint(address, portNumber);
+
+
+            Socket nodeSocket = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                nodeSocket.Connect(serverEndPoint);
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine(e.Message);
+                return;
+            }
+            this.rasp = nodeSocket;
+        }
+    }
+
+    class JsonData
+    {
+        public double ProcessedData { get; set; }
+        public List<double> SensorData { get; set; }
+
+        override public string ToString(){
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"procesed Value{this.ProcessedData}\n [");
+            this.SensorData.ForEach(d => sb.Append($"{d}"));
+            sb.Append("]\n");
+            return sb.ToString();
         }
     }
 
--- scada_server/index.js
+++ scada_server/index.js
@@ -1,13 +1,79 @@
 const express = require('express')
 const app = express()
+const net = require('net')
 const port = 5000
 
+let sensor1Data = []
+let sensor2Data = []
+let sensor3Data = []
+let sensorProcessedData = []
+
+const dataServer = net.createServer(socket => {
+    console.log('witam');
+
+    socket.on('data', function (chunk) {
+        let data = JSON.parse(chunk.toString('utf8'))
+
+        sensorProcessedData.push(data['ProcessedData'])
+        if(sensorProcessedData.length > 500){
+            sensorProcessedData.shift();
+        }
+
+        let sarr = data['SensorData'];
+        console.log(sarr.length);
+        if(sarr.length == 1){
+            console.log('elo',sarr[0]);
+            sensor1Data.push(sarr[0]);
+            if(sensor1Data > 500){
+                sensor1Data.shift();
+            }
+        }
+        else if(sarr.lengt == 2){
+            console.log('elo',sarr[0]);
+            console.log('elo2',sarr[1]);
+        }
+        else if(sarr.lengt == 3){
+            console.log('elo',sarr[0]);
+            console.log('elo2',sarr[1]);
+            console.log('elo3',sarr[2]);
+        }
+
+        // if(sarr.lengt == 2){
+        //     console.log('elo2',sarr[1]);
+        //     sensor2Data.push(sarr[1]);
+        //     if(sensor2Data > 500){
+        //         sensor2Data.shift();
+        //     }
+        // };
+
+        // if(sarr.lengt >= 3){
+        //     console.log('elo3',sarr[2]);
+        //     sensor3Data.push(sarr[2]);
+        //     if(sensor3Data > 500){
+        //         sensor3Data.shift();
+        //     }
+        // };
+    });
+})
 app.use(express.static('../scada_display/build'))
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/data', (req, res) => {
-    console.log('data was requested');
-    res.send('server data here')
-})
 
+    console.log("data requested");
+    let jsonData = {
+        main: [...sensorProcessedData],
+        sensor1: [...sensor1Data],
+        sensor2: [...sensor2Data],
+        sensor3: [...sensor3Data],
+    };
+    
+    sensor1Data = [];
+    sensor2Data = [];
+    sensor3Data = [];
+    sensorProcessedData = [];
+
+    res.json(JSON.stringify(jsonData));
+})
+dataServer.listen(1337, '127.0.0.1')
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
